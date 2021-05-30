d315eaa49335af7feff99903069b9797aba20b56
Augusto Fadanelli de Sousa <augustofadanelli.work@gmail.com>
2021-05-30 02:53:31 -0300

    Create TankBullet.java

---
+++ TankBullet.java
@@ -0,0 +1,16 @@
+/*
+* Battle tank bullets features
+*/
+
+public class TankBullet
+{
+	private int shootRate = 0;
+
+	public void setShootRate(int s){
+		this.shootRate = s;
+	}
+	public int getShootRate(){
+		return this.shootRate;
+	}
+}
+
